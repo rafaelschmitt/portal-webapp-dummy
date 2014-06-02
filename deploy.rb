fce3334eba917ef18ba2581f8db201105252700b
rafaelschmitt <rafael.schmitt@hs-augsburg.de>
2014-06-02 16:07:28 +0200

    fixed bug in deploy.rb

--- deploy.rb
+++ deploy.rb
@@ -1,4 +1,4 @@
 # set current link
-src = "/home/kokoloko/projects/sandbox/liveserver/beiuns/software/portal-webapp-dummy/releases/v3.0.0/index.html"
+src = "/home/kokoloko/projects/sandbox/liveserver/beiuns/software/portal-webapp-dummy/releases/v3.1.0/index.html"
 dst = "/home/kokoloko/projects/sandbox/liveserver/beiuns/software/portal-webapp-dummy/current/index.html"
 system 'ln', '-s', '-f', src, dst
