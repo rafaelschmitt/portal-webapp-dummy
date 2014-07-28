25e53dba8a1eb9f0ef6aa29c5e1613c5b26ac03b
rafaelschmitt <rafael.schmitt@hs-augsburg.de>
2014-07-28 11:32:38 +0200

    edited deploy.rb

--- deploy.rb
+++ deploy.rb
@@ -1,19 +1,17 @@
 # postsetup tasks
-newTask (
-	"database", 
-	"do database stuff blablado database stuff blablado database stuff blablado database stuff ", 
-	[
-		"php app/console doctrine:database:create", 
-		"php app/console doctrine:schema:update --dump-sql", 
-		"php app/console doctrine:schema:update --force", 
-		"echo 'y' | php app/console doctrine:fixtures:load"
-	]
-)
+task = newTask
+task.title = "database"
+task.desc = "do database stuff"
+task.commands = [
+	"php app/console doctrine:database:create", 
+	"php app/console doctrine:schema:update --dump-sql", 
+	"php app/console doctrine:schema:update --force", 
+	"echo 'y' | php app/console doctrine:fixtures:load"	
+]
 
-newTask (
-	"other stuff", 
-	"just do this other stuff", 
-	[
-		"otherstuff command1", 
-	]
-)
+task = newTask
+task.title = "other stuff"
+task.desc = "just do the other stuff"
+task.commands = [
+	"php otherstuff"
+]
